eaacd6ddda9e4be07f94cf82d5749685713d74a3
Rohit Jadhav <rohitjadhav8698@gmail.com>
2022-05-27 09:18:22 +0530

    media queries setup

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const accordionItem = document.querySelectorAll("#accordion-item");
 
+// accordion item selection
 accordionItem.forEach((item) => {
   const triggerOpen = item.querySelector(".trigger");
 
+  // event listner
   triggerOpen.addEventListener("click", () => {
     const openItem = document.querySelector('.accordion-open')
 
     toggleItem(item);
 
-    if(openItem && openItem!== item){
+    if(openItem && openItem !== item){
       toggleItem(openItem)
     }
   });
 });
 
+// trigger function
 const toggleItem = (item) => {
   if(item.classList.contains('accordion-open')){
     item.classList.remove('accordion-open')
